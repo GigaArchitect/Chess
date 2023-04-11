8d3a91316cbb7544f0f70900d0e76bb57620033b
Mustafa <nsar90554046@gmail.com>
2023-04-11 22:34:25 +0200

    Made kngiht moves valid

--- knight.py
+++ knight.py
@@ -7,18 +7,44 @@ class knight :
         moves = []
         ch = current_pos[0]
         nm = int(current_pos[1])
-        #Right
-        moves.append(f"{pos_letters[pos_letters.index(ch)+2]}{nm+1}")
-        moves.append(f"{pos_letters[pos_letters.index(ch)+2]}{nm-1}")
-        #left
-        moves.append(f"{pos_letters[pos_letters.index(ch)-2]}{nm+1}")
-        moves.append(f"{pos_letters[pos_letters.index(ch)-2]}{nm-1}")
-        #diag-left-forward
-        moves.append(f"{pos_letters[pos_letters.index(ch)+1]}{nm+2}")
-        moves.append(f"{pos_letters[pos_letters.index(ch)-1]}{nm+2}")
-        #diag-right-forward
-        moves.append(f"{pos_letters[pos_letters.index(ch)+1]}{nm-2}")
-        moves.append(f"{pos_letters[pos_letters.index(ch)-1]}{nm-2}")
+        if pos_letters.index(ch)+2 > 7 or nm+1 > 8:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)+2]}{nm+1}")
+        if pos_letters.index(ch)+2 > 7 or nm-1 < 1:
+            pass
+        else :    
+            moves.append(f"{pos_letters[pos_letters.index(ch)+2]}{nm-1}")
+        
+        if pos_letters.index(ch)-2 < 0 or nm+1 > 8:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)-2]}{nm+1}")
+
+        if pos_letters.index(ch)-2 < 0 or nm-1 > 1:
+            pass
+        else :    
+            moves.append(f"{pos_letters[pos_letters.index(ch)-2]}{nm-1}")
+        
+        if pos_letters.index(ch)+1 > 7 or nm+2 > 8:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)+1]}{nm+2}")
+        
+        if pos_letters.index(ch)-1 < 0 or nm+2 > 8:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)-1]}{nm+2}")
+        
+        if pos_letters.index(ch)+1 > 7 or nm-2 < 1:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)+1]}{nm-2}")
+
+        if pos_letters.index(ch)-1 < 0 or nm-2 < 1:
+            pass
+        else :
+            moves.append(f"{pos_letters[pos_letters.index(ch)-1]}{nm-2}")
 
         return moves
 
